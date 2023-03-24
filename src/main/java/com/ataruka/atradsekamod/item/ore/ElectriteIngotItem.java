c5001888a6966d2becc765b991f05cabc32ac5a1
ataRuka <yuk3034@gmail.com>
2023-03-24 17:11:00 +0900

    アイテム名の色を紫色に変えました

--- src/main/java/com/ataruka/atradsekamod/item/ore/ElectriteIngotItem.java
+++ src/main/java/com/ataruka/atradsekamod/item/ore/ElectriteIngotItem.java
@@ -3,10 +3,11 @@ package com.ataruka.atradsekamod.item.ore;
 import com.ataruka.atradsekamod.init.ItemGroupInit;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
 
 public class ElectriteIngotItem extends Item{
 
 	public ElectriteIngotItem() {
-		super(new Properties().tab(ItemGroupInit.ATRADSEKA_MOD));
+		super(new Properties().tab(ItemGroupInit.ATRADSEKA_MOD).rarity(Rarity.EPIC));
 	}
 }
